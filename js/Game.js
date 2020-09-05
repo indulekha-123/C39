0de1b8519670ef356ccaf4076e286c4fb7c4a605
indulekha-123 <indulekha4coding@gmail.com>
2020-09-05 10:33:19 +0530

    car racing

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,17 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1_img);
+
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2_img);
+
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3_img);
+
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4_img);
+
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +50,16 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background (198,135,103);
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
+
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -56,13 +67,17 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 250;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke (10);
+          fill ("red");
+          ellipse(x,y,60,60);
+          
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
@@ -78,7 +93,18 @@ class Game {
       player.distance +=10
       player.update();
     }
+if(player.distance>3860){
+  gameState=2;
+
+}
 
     drawSprites();
   }
+end(){
+  console.log("game end");
+  game.update(2);
+  
+}
+
+
 }
